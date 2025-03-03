4074216cd9071b9c9eb446d473ccedf3a896bffa
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-03-03 15:30:52 +0100

    add env

--- api/apiConfig.js
+++ api/apiConfig.js
@@ -1,36 +1,40 @@
-import axios from 'axios';
-const API_URL =
-  import.meta.env.VITE_API_URL 
-  //  || "https://be-kapusta-team-project.onrender.com"
+import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
 console.log("API URL:", API_URL);
 const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+	baseURL: API_URL,
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
-// Dodanie interceptorów 
+// Dodanie interceptorów
 api.interceptors.response.use(
-  response => response,
-  async error => {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        const refreshToken = localStorage.getItem('refreshToken');
-        const { data } = await axios.post(`${API_URL}/refresh-token`, { token: refreshToken });
-        
-        localStorage.setItem('token', data.accessToken); originalRequest.headers['Authorization'] = `Bearer ${data.accessToken}`;
-        return api(originalRequest);
-      } catch (err) {
-        console.error('Unable to refresh token', err);
-        // Wyloguj użytkownika, jeśli nie uda się odświeżyć tokena
-        localStorage.removeItem('token');
-        localStorage.removeItem('refreshToken');
-        window.location.href = '/login';
-      }
-    } return Promise.reject(error);
-  }
+	(response) => response,
+	async (error) => {
+		const originalRequest = error.config;
+		if (error.response.status === 401 && !originalRequest._retry) {
+			originalRequest._retry = true;
+			try {
+				const refreshToken = localStorage.getItem("refreshToken");
+				const { data } = await axios.post(`${API_URL}/refresh-token`, {
+					token: refreshToken,
+				});
+
+				localStorage.setItem("token", data.accessToken);
+				originalRequest.headers[
+					"Authorization"
+				] = `Bearer ${data.accessToken}`;
+				return api(originalRequest);
+			} catch (err) {
+				console.error("Unable to refresh token", err);
+				// Wyloguj użytkownika, jeśli nie uda się odświeżyć tokena
+				localStorage.removeItem("token");
+				localStorage.removeItem("refreshToken");
+				window.location.href = "/login";
+			}
+		}
+		return Promise.reject(error);
+	}
 );
 
 export default API_URL;
